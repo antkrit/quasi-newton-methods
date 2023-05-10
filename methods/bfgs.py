c7741f862e5b2beeed738b9be50421524ba0abe4
Anton Krytskyi <anton_krytskyi@epam.com>
2023-05-10 20:54:10 +0300

    BFGS method

---
+++ methods/bfgs.py
@@ -0,0 +1,53 @@
+"""BFGS minimization method."""
+from typing import Callable
+
+import numpy as np
+
+from methods.base import QuasiNewton
+
+
+class BFGS(QuasiNewton):
+    """Broyden-Fletcher-Goldfarb-Shanno minimization method.
+
+    Examples:
+        Define objective function
+
+        >>> def obj_func(x):
+        ...     return x**2
+
+        Initialize minimizer and minimize point x
+
+        >>> minimize = BFGS()
+        >>> min_ = minimize(np.array([1, -1.5]), obj_func, None, maxiter=100, eps=1e-6)
+        >>> np.allclose(min_, [0, 0], atol=1e-6)
+        True
+
+    References:
+        1. Jorge Nocedal, `Numerical Optimization. Second Edition`, 2006, p.140
+    """
+
+    def update(
+        self,
+        x: np.ndarray,
+        f: Callable[[np.ndarray], np.ndarray],
+        df: Callable[[np.ndarray], np.ndarray],
+        *args,
+        **kwargs,
+    ) -> np.ndarray:
+        dx, dy = super().update(x, f, df, *args, **kwargs)
+
+        identity = np.identity(len(x))
+
+        np.dot(
+            identity - (dx.dot(dy.T) / dy.T.dot(dx)),
+            self.hessian,
+            out=self.hessian,
+        )
+        np.dot(
+            self.hessian,
+            identity - (dy.dot(dx.T) / dy.T.dot(dx)),
+            out=self.hessian,
+        )
+        np.add(self.hessian, dx.dot(dx.T) / dy.T.dot(dx), out=self.hessian)
+
+        return dx.flatten()
--- tests/test_methods.py
+++ tests/test_methods.py
@@ -2,13 +2,14 @@
 import numpy as np
 import pytest
 
+from methods.bfgs import BFGS
 from methods.broyden import Broyden
 from methods.dfp import DFP
 from tests.utils.array import allclose
 from tests.utils.functions import parabola, parabola_derivative
 
 
-@pytest.mark.parametrize("obj_method", [DFP, Broyden], ids=["DFP", "Broyden"])
+@pytest.mark.parametrize("obj_method", [DFP, Broyden, BFGS], ids=["DFP", "Broyden", "BFGS"])
 @pytest.mark.parametrize("x", [np.ones(2), np.ones(3)], ids=["2elem.", "3elem."])
 def test_convergence(obj_method, x):
     """Test convergence of methods."""
